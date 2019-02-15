1f14baaa9a63fd8c597c9e278e65a672ebc68390
Marifel Angeles <mmarifel@gmail.com>
2019-02-15 07:21:12 -0600

    add post route to add new koalas

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -8,7 +8,18 @@ const koalaRouter = express.Router();
 
 
 // POST
-
+app.post('/', (req, res) => {
+    console.log('sending via POST', req.body);
+    // expect koalaToSend {name, age, gender, readyForTransfer, notes }
+    pool.query(`INSERT INTO "koalas" ("name", "age", "gender", "readyForTransfer", "notes") 
+    VALUES ( $1, $2, $3, $4, $5 );`, [req.body.name, req.body.age, req.body.gender, req.body.readyForTransfer, req.body.notes])
+        .then(() => {
+            res.sendStatus(200);
+        }).catch((error) => {
+            console.log('error with koalas insert', error);
+            res.sendStatus(500);
+        });
+});
 
 // PUT
 
