bb453021fac5cba5f305d4820f4b5538d3edafb7
Marifel Angeles <mmarifel@gmail.com>
2019-02-15 07:09:12 -0600

    add get route to get all koalas

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -4,7 +4,20 @@ const koalaRouter = express.Router();
 // DB CONNECTION
 
 
-// GET
+
+// GET all koalas 
+router.get('/', (req, res) => {
+    console.log('GET /koala');
+    // expect koalaToSend { name, age, gender, readyForTransfer, notes }
+    pool.query(`SELECT * FROM "koalas" ORDER BY "id";`)
+        .then((results) => {
+            console.log('results from select', results.rows);
+            res.send(results.rows);
+        }).catch((error) => {
+            console.log('error with koalas select', error);
+            res.sendStatus(500);
+        });
+});
 
 
 // POST
